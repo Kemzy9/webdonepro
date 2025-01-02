da64631ebfe3e8482ca0acfc83db1dce84a40bb1
nain <112144627+Kemzy9@users.noreply.github.com>
2025-01-02 11:08:16 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 
 import React from "react";
-import Head from "@/app/UI/head/page"
-import Hero from "@/app/UI/Hero/page"
-import Section from "@/app/UI/section/paeg"
+import Head from "@/app/UI/head/page";
+import Hero from "@/app/UI/Hero/page";
+import Section from "@/app/UI/section/paeg";
 import Footer from './UI/footer/page';
-import Feature from "@/app/UI/feature/page"
-import Features from "@/app/UI/Features/page"
+import Feature from "@/app/UI/feature/page";
+import Features from "@/app/UI/Features/page";
 
 export default function Home() {
   return (
