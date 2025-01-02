bae728ddd2b2daeaa4615ef343ff23b788e5031e
nain <112144627+Kemzy9@users.noreply.github.com>
2025-01-02 10:23:38 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Head from "@/app/UI/head/page"
 import Hero from "@/app/UI/Hero/page"
 import Section from "@/app/UI/section/paeg"
-
+import Footer from './UI/footer/page';
 import Feature from "@/app/UI/feature/page"
 import Features from "@/app/UI/Features/page"
 
@@ -17,7 +17,7 @@ export default function Home() {
     <Feature />
     <Features />
     <Section />
-
+    <Footer />
 
 
 
